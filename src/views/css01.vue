a994c7b7047a5aa563ec7aa58902baff916e9e44
liangkaixi <liangkaixi@163.com>
2024-08-03 21:42:55 +0800

    css_100_01_over

--- src/views/css01.vue
+++ src/views/css01.vue
@@ -6,38 +6,57 @@
             This is simply unbelievable! I would be lost without Apple. The very
             best. Not able to tell you how happy I am with Apple.
         </p>
-        <h4 class="username">Cheriser G</h4>
+        <h3 class="username">Cheriser G</h3>
     </div>
+
 </template>
 <script setup>
 </script>
 <style scoped>
-body {
+.body {
     margin: 0;
     display: flex;
+    justify-content: center;
     height: 100vh;
     align-items: center;
     font-family: cursive;
+    overflow-x: hidden;
 }
-.testimonial-container{
+
+.testimonial-container {
     width: 500px;
-    height: 100px;
+    height: 200px;
     background-color: slateblue;
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-    min-width: 400px;
-    padding: 70px 20px;
+    position: relative;
+    padding: 50px 20px;
     margin: 5px;
     color: white;
-    position: relative;
+    box-sizing: border-box;
 }
-img{
+
+.username {
+    margin-top: 20px;
+}
+
+img {
     width: 100px;
     height: 100px;
     border-radius: 50%;
     position: absolute;
-    left: 50%;
+    top: -30%;
     transform: translateX(-50%);
-    top: -50%;
+    left: 50%;
+
+
+}
+
+@media (max-width:720px) {
+    .testimonial-container {
+        margin-top: 60px;
+        width: 100%;
+        box-sizing: border-box;
+    }
 }
 </style>
